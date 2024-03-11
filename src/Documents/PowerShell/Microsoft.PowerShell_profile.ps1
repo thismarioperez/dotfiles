15747aeb979d8e1bf22835a31537e68aa56422dd
Mario Perez <mario.perez415@gmail.com>
2024-03-11 02:13:30 -0400

    Update Microsoft.PowerShell_profile.ps1

--- src/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,28 @@ $env:XDG_DATA_HOME = "$HOME\.local\share"
 
 # Load oh-my-posh
 oh-my-posh init pwsh --config "$env:XDG_DATA_HOME\oh-my-posh\themes\thismarioperez.omp.json" | Invoke-Expression
+
+# Load terminal icons
+# Run this to install the module: 
+# ```
+# Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+#```
+Import-Module -Name Terminal-Icons
+
+# Make it easy to edit this profile once it's installed
+# We're only targeting the current user, all hosts profile
+function Edit-Profile {
+    if ($host.Name -match "ise") {
+        $psISE.CurrentPowerShellTab.Files.Add($PROFILE.CurrentUserAllHosts)
+    } else {
+        code $PROFILE.CurrentUserAllHosts
+    }
+}
+
+# Easy reload profile
+function Reload-Profile {
+    & $PROFILE.CurrentUserAllHosts
+}
+
+# Set UNIX-like aliases for the list command
+function ll { Get-ChildItem -Path $pwd -File }
